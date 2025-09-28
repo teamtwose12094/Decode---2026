73c4ff0155f1906bfc50e4df7b2865819bb1490b
teamtwose12094 <teamtwose12094@gmail.com>
2025-09-28 15:16:15 -0600

    Added code to launch Artifacts

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagsTest.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+
+@TeleOp(name = "AprilTagsTest",group = "TeleOp")
+public class AprilTagsTest extends LinearOpMode {
+
+    final double DESIRED_DISTANCE = 6.0; // this is how close the camera should get to the target (inches)
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
@@ -13,12 +13,16 @@ public class DriverControl extends LinearOpMode {
     private DcMotor backLeftDrive = null;
     private DcMotor frontRightDrive = null;
     private DcMotor backRightDrive = null;
+    private DcMotor Launcher = null;
 
     Config config = new Config();
 
-    //Drive Motor Paramaters
+    //Drive Motor Parameters
     double driveSpeedMultiplier = config.driveSpeedMultiplier;
     double pivotSpeedMultiplier = config.pivotSpeedMultiplier;
+
+    private boolean isSlowMode = false;
+    private boolean lastRightBumperState = false;
 
 
 
@@ -31,48 +35,82 @@ public class DriverControl extends LinearOpMode {
         frontRightDrive = hardwareMap.get(DcMotor.class,"frontRightDrive");
         backRightDrive = hardwareMap.get(DcMotor.class,"backRightDrive");
 
+        Launcher = hardwareMap.get(DcMotor.class,"Launcher");
+
         frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         backRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        frontLeftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontRightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
-        backRightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontLeftDrive.setDirection(DcMotorSimple.Direction.REVERSE); //Test to make sure is right direction
+        backLeftDrive.setDirection(DcMotorSimple.Direction.REVERSE); //Test to make sure is right direction
+        frontRightDrive.setDirection(DcMotorSimple.Direction.FORWARD); //Test to make sure is right direction
+        backRightDrive.setDirection(DcMotorSimple.Direction.FORWARD); //Test to make sure is right direction
+
+        Launcher.setDirection(DcMotorSimple.Direction.FORWARD); //Test to make sure is right direction
 
         waitForStart();
         runtime.reset();
 
-        //Run until end of match (Driver Press Stop)
+
+        //Slow Mode
+
+        boolean currentRightBumperState = gamepad1.right_bumper;
 
 
+        if (currentRightBumperState && !lastRightBumperState) {
+            // Toggle the slow mode state
+            isSlowMode = !isSlowMode;
+        }
+
+
+        if (isSlowMode) {
+            // SLOW MODE ACTIVE
+            driveSpeedMultiplier = config.slowDriveSpeedMultiplier;
+            pivotSpeedMultiplier = config.slowPivotSpeedMultiplier;
+        } else {
+            // NORMAL MODE ACTIVE
+            driveSpeedMultiplier = config.driveSpeedMultiplier;
+            pivotSpeedMultiplier = config.pivotSpeedMultiplier;
+        }
+
+
+
+        //Driving
         //Use left joystick to go forward & strafe, right joystick to turn/rotate
         double axial = -gamepad1.left_stick_y * driveSpeedMultiplier;  // Note: pushing stick forward gives negative value
         double lateral = gamepad1.left_stick_x * driveSpeedMultiplier;
         double yaw = gamepad1.right_stick_x * pivotSpeedMultiplier;
 
-
-        // Normalize the values so no wheel power exceeds 100%
-        // This ensures that the robot maintains the desired motion.
-        max = Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower));
-        max = Math.max(max, Math.abs(backLeftPower));
-        max = Math.max(max, Math.abs(backRightPower));
-
-        if (max > 1.0) {
-            frontLeftPower /= max;
-            frontRightPower /= max;
-            backLeftPower /= max;
-            backRightPower /= max;
-        }
-
-        // Send calculated power to wheels
-        frontLeftDrive.setPower(frontLeftPower);
-        frontRightDrive.setPower(frontRightPower);
-        backLeftDrive.setPower(backLeftPower);
-        backRightDrive.setPower(backRightPower);
+        //Launcher
+       double triggerValue = gamepad2.left_trigger;
+       if (triggerValue > 0.1) {
+           // If the trigger is fully pressed (1.0), the motor runs at full power.
+           Launcher.setPower(triggerValue);
+           telemetry.addData("Launch Motor", "Running @ " + triggerValue);
+       } else {
+           // If the trigger is released (value <= 0.1), stop the motor.
+           Launcher.setPower(0.0);
+           telemetry.addData("Launch Motor", "Stopped");
+       }
 
 
+        double max = 0.0;
 
+        //Telemetry
+        telemetry.addData("wheel", frontLeftDrive.getCurrentPosition());
+
+
+        telemetry.addData("Player 1 Controls","");
+        telemetry.addData("Right Bumper", "Slow Mode");
+        telemetry.addData("Left Stick Up", "Drive Forwards");
+        telemetry.addData("Left Stick Down", "Drive Backwards");
+        telemetry.addData("Left Stick Left", "Strafe Left");
+        telemetry.addData("Left Stick Right", "Strafe Right");
+        telemetry.addData("Right Stick Left", "Turn Left");
+        telemetry.addData("Right Stick Right", "Turn Right");
+
+        telemetry.addData("Player 2 Controls","");
+        telemetry.addData("Left Trigger", "Launch Artifacts");
     }
 }
